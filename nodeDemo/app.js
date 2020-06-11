d09b49f53ca524efaffa3dd25bfcc12f25a68ac8
zyong <997610162@qq.com>
2020-06-11 21:21:54 +0800

    6.11在家提交2

--- nodeDemo/app.js
+++
@@ -1,8 +0,0 @@
-// 定时器
-// setTimeout(() =>{
-//     console.log(__dirname) //不包含当前文件名的路径
-//     console.log(__filename)//包含当前文件名的路径
-// },3000)
-const stuff = require('./module');
-console.log(stuff.counter([1,2,3]))
-console.log(stuff.add(1,stuff.num))
